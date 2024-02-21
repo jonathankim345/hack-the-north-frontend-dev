93d4e6850302ab6eeee55df2fced39b4c4d2d3ac
jonathankim345 <jonkim345@gmail.com>
2024-02-21 01:41:51 -0500

    vite config

--- frontend/create-react-app/vite.config.js
+++ frontend/create-react-app/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: 'esnext',
+    platform: 'linux',
+  },
   server: {
     proxy: {
       '/api': {
