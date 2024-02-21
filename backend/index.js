961265b7c5e6dc576168472e894d3a4bce89a9ec
jonathankim345 <40842083+jonathankim345@users.noreply.github.com>
2024-02-21 09:36:06 -0500

    Added additional origin to CORS

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const { EMAIL, PASSWORD } = process.env;
 const PORT = process.env.PORT || 3000; // Use the dynamic port assigned by Vercel or default to 3000
 
 app.use(cors({
-    origin: ["https://hack-the-north-frontend-dev.vercel.app", "https://hack-the-north-frontend-dev-api.vercel.app/api/login"],
+    origin: ["https://hack-the-north-frontend-dev.vercel.app", "https://hack-the-north-frontend-dev.vercel.app/login"],
     methods: ["POST", "GET"],
     credentials: true
 }));
