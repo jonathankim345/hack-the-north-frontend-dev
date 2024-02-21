53ed8d8e4cb8c6ad42a21ed7c3689fb617dafb3e
jonathankim345 <40842083+jonathankim345@users.noreply.github.com>
2024-02-21 09:59:58 -0500

    Changed routing

--- frontend/create-react-app/src/main.jsx
+++ frontend/create-react-app/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Route, HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import Login from './pages/Login.jsx';
 import './index.css';
@@ -21,8 +21,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={router}>
+      <HashRouter>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
+        </HashRouter>
       </RouterProvider>
     </AuthProvider>
   </React.StrictMode>
