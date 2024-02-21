b7d92bfa1e648b3b770a286315891d15a6d3466b
jonathankim345 <jonkim345@gmail.com>
2024-02-21 01:59:40 -0500

    updating packages

--- frontend/create-react-app/vite.config.js
+++ frontend/create-react-app/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['bootstrap'],
+  },
   esbuild: {
     target: 'esnext',
   },
